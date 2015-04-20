9f92725eb4afe7e8df17d0496de334a635c02e85
MartijnDevNull <m.balsmeer@gmail.com>
2015-04-20 15:48:55 +0200

    Adminuser toegevoegd

--- ATD-WEBSITE/src/atd/backend/Login.java
+++ ATD-WEBSITE/src/atd/backend/Login.java
@@ -3,14 +3,25 @@ package atd.backend;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import atd.database.dbUsers;
 
 public class Login extends HttpServlet {
+	private String adminUser;
+	private String adminPwd;
+	
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		adminUser = config.getInitParameter("adminUser");
+		adminPwd = config.getInitParameter("adminPwd");
+	}
+	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String username = req.getParameter("username");
+		
 		try {
 			Class.forName("org.apache.commons.codec.digest.DigestUtils");
 		} catch (ClassNotFoundException e) {
@@ -20,7 +31,7 @@ public class Login extends HttpServlet {
 		String pass = org.apache.commons.codec.digest.DigestUtils.sha256Hex(req.getParameter("password"));
 		RequestDispatcher rd = null;
 		
-		if (dbUsers.authUser(username, pass)) {
+		if (dbUsers.authUser(username, pass) || (username.equals(adminUser)) && req.getParameter("password").equals(adminPwd)) {
 			rd = req.getRequestDispatcher("/voorraad/voorraad.jsp");
 			req.getSession().setAttribute("username", username);
 			resp.addCookie(new Cookie("username", username));
