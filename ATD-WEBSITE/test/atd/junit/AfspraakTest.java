a7f53dbd13acc7479b63b7ad64a8c99673dca08f
klaas <klaas.foppen@gmail.com>
2015-06-06 11:02:55 +0200

    junit tests nu ook een skelet

---
+++ ATD-WEBSITE/test/atd/junit/AfspraakTest.java
@@ -0,0 +1,20 @@
+package atd.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AfspraakTest {
+
+	@Before
+	public void start() {
+
+	}
+
+	@Test
+	public void testFiets() {
+
+	}
+
+}
