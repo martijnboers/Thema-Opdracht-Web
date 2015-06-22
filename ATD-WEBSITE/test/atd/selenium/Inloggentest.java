43533e367501c4579759da4f196024ef3196f3bb
Meesgieling <meesgieling@live.nl>
2015-06-22 15:48:18 +0200

    test

--- ATD-WEBSITE/test/atd/selenium/Inloggentest.java
+++ ATD-WEBSITE/test/atd/selenium/Inloggentest.java
@@ -1,17 +1,3 @@
-/*******************************************************************************
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 package atd.selenium;
 
 import static org.junit.Assert.fail;
@@ -32,7 +18,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class RegisterUser {
+public class Inloggentest {
 	private WebDriver driver;
 	private String baseUrl;
 	private boolean acceptNextAlert = true;
@@ -41,13 +27,13 @@ public class RegisterUser {
 	@Before
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
-		baseUrl = "https://atd.plebian.nl/";
+		baseUrl = "http://localhost:8080";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void testCsv() throws Exception {
-		String csvFile = "test/atd/selenium/RegisterUser.csv";
+		String csvFile = "test/atd/selenium/Inloggenuser.csv";
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ",";
@@ -60,24 +46,12 @@ public class RegisterUser {
 				// use comma as separator
 				String[] entry = line.split(cvsSplitBy);
 
-				driver.get(baseUrl + "/ATD-WEBSITE/register/register.jsp");
+				driver.get(baseUrl + "/ATD-WEBSITE/index.jsp");
 				driver.findElement(By.name("username")).clear();
 				driver.findElement(By.name("username")).sendKeys(entry[0]);
 				driver.findElement(By.name("password")).clear();
-				driver.findElement(By.name("password")).sendKeys(entry[2]);
-				driver.findElement(By.name("realname")).clear();
-				driver.findElement(By.name("realname")).sendKeys(entry[0] + " " + entry[1]);
-				driver.findElement(By.name("postcode")).clear();
-				driver.findElement(By.name("postcode")).sendKeys(entry[3]);
-				driver.findElement(By.name("email")).clear();
-				driver.findElement(By.name("email")).sendKeys(entry[4]);
-				driver.findElement(By.name("kenteken")).clear();
-				driver.findElement(By.name("kenteken")).sendKeys(entry[5]);
-				driver.findElement(By.name("merk")).clear();
-				driver.findElement(By.name("merk")).sendKeys(entry[6]);
-				driver.findElement(By.name("type")).clear();
-				driver.findElement(By.name("type")).sendKeys(entry[7]);
-				driver.findElement(By.xpath("//input[@value='Registreren']")).click();
+				driver.findElement(By.name("password")).sendKeys(entry[1]);
+				driver.findElement(By.xpath("//input[@value='aanmelden']")).click();
 
 			}
 
---
+++ ATD-WEBSITE/test/atd/selenium/onderdeeltest.java
@@ -0,0 +1,5 @@
+package atd.selenium;
+
+public class onderdeeltest {
+
+}
